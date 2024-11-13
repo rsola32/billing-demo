6f855f5158f3c08904bb6256992bbdfeacca5049
Rajesh Sola <rsola@gitam.edu>
2024-11-13 13:29:13 +0530

    Create BillingDemo.java

---
+++ src/main/java/org/gitam/cseblr/BillingDemo.java
@@ -0,0 +1,9 @@
+package org.gitam.cseblr;
+import org.gitam.cseblr.lib.CustomerList;
+
+public class BillingDemo {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");        
+    }
+}
